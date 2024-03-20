e65e212824df52b258d06345c59a76dd41b07161
NitteshKhanna <nitteshkhanna19@gmail.com>
2024-03-21 04:50:03 +0530

    almost have completed the v1

--- client/src/app/page/page.component.ts
+++ client/src/app/page/page.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit, OnDestroy } from '@angular/core';
+import { log } from 'node:console';
 
 @Component({
   selector: 'app-page',
@@ -10,45 +11,19 @@ import { Component, OnInit, OnDestroy } from '@angular/core';
 export class PageComponent {
   carCounts: { [feedId: number]: number } = {};
   private intervalIds: any[] = [];
+  serverLink: string = "http://localhost:5000"
+  signalCounterValues: number[] = [1, 2, 3, 4]
+  timer: any;
+  seconds: number = 0;
+  signalDuration: number = 30;
+  trafficLights: HTMLElement[] = [];
+  pointer: number = 0;
+  order: number[] = [1, 2, 3, 4];
 
   constructor() { }
-  serverLink: string = "http://127.0.0.1:5000"
+  
   startDetection(): void {
     fetch(this.serverLink + '/start_all_feeds', {
-      method: 'POST',
-      headers: {
-        'Content-Type': 'application/json'
-      },
-      body: JSON.stringify({})
-    })
-    .then(response => response.json())
-    .then(data => {
-      console.log(data);
-    })
-    .catch(error => {
-      console.error('Error:', error);
-    });
-  }
-
-  stopDetection(): void {
-    fetch(this.serverLink + '/stop_detection', {
-      method: 'POST',
-      headers: {
-        'Content-Type': 'application/json'
-      },
-      body: JSON.stringify({})
-    })
-    .then(response => response.json())
-    .then(data => {
-      console.log(data);
-    })
-    .catch(error => {
-      console.error('Error:', error);
-    });
-  }
-
-  restartDetection(): void {
-    fetch(this.serverLink + '/restart_detection', {
       method: 'POST',
       headers: {
         'Content-Type': 'application/json'
@@ -67,31 +42,63 @@ export class PageComponent {
   ngOnInit(): void
   {
     
+    this.trafficLights = [
+      (<HTMLElement>document.getElementById("tra1")),
+      (<HTMLElement>document.getElementById("tra2")),
+      (<HTMLElement>document.getElementById("tra3")),
+      (<HTMLElement>document.getElementById("tra4"))
+    ]
+
     document.querySelector(".start-cta")?.addEventListener("click", ()=>{
       (<HTMLElement>document.querySelector(".start-cta")).style.display = "none";
       this.startDetection();
       this.startFetchingCarCounts();
+      this.setGreenLight(0);
+      this.startTimer();
     (<HTMLElement>document.querySelector(".traffic-panel")).style.display = "flex";
 
     });
     (<HTMLElement>document.querySelector(".traffic-panel")).style.display = "none";
   }
-  ngOnDestroy() {
-    this.intervalIds.forEach(clearInterval);
-    
-  }
+  
   startFetchingCarCounts() {
-    const feedCount = 4; // Assuming there are 4 feeds
+    const feedCount = 4; 
+    let carCounts: number[][] = [];
     for (let feedId = 1; feedId <= feedCount; feedId++) {
-      const intervalId = setInterval(() => {
+      
         fetch(`http://localhost:5000/car_count_feed_${feedId}`)
           .then(response => response.json())
           .then(data => {
             this.carCounts[feedId] = data.car_count;
+            carCounts.push([feedId, data.car_count])
           })
           .catch(err => console.error(`Error fetching car count for feed ${feedId}:`, err));
-      }, 3000); // Update car count every 3 seconds
-      this.intervalIds.push(intervalId);
     }
+  }
+
+  
+  startTimer() {
+    this.timer = setInterval(() => {
+      this.signalCounterValues = this.signalCounterValues.map (time => {
+        if(time == 0)
+         {
+          this.nextSignal();
+          return 4 * this.signalDuration;
+         }
+         return time - 1;
+      } );
+    }, 1000);
+  }
+
+  setGreenLight(light: number) { 
+    this.trafficLights.map(div => div.style.background = "linear-gradient(to right bottom, #d32628, red)");
+    this.trafficLights[light].style.background = "linear-gradient(to right bottom, #0adb8b, green)";
+  }
+
+  nextSignal() {
+    this.setGreenLight(this.order [(this.pointer == 3)? 0: ++this.pointer]);
+  }
+  ngOnDestroy() {
+    clearInterval(this.timer);
   }
 }
